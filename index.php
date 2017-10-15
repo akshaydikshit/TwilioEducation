78da2d8ea064d8bc215651b75dc0622c6f845816
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 18:24:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,33 +3,33 @@ require_once './autoload.php';
 use Twilio\Twiml;
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
-if( ($from == '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+if( ($from == '+19292402278') && (stripos($body, '12345' )!== FALSE) && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Your ward was Present on this date.");
 }
-else if( ($from == '+19292402278') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
- $response = new Twiml();
-$message = $response->message("Your ward was Present on this date.");
-}
-
-else if( ($from == '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+else if( ($from == '+19292402278')  && (stripos($body, '12345' )!== FALSE) && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Your ward was Present on this date.");
 }
 
-else if( ($from != '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+else if( ($from == '+19292402278') && (stripos($body, '12345' )!== FALSE) && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Your ward was Present on this date.");
+}
+
+else if( ($from != '+19292402278') && (stripos($body, '12345' )!== FALSE) && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
 }
 
-else if( ($from == '+19292402278') && (stripos($body, '2017-10-04' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+else if( ($from == '+19292402278') && (stripos($body, '12345' )!== FALSE) && (stripos($body, '2017-10-04' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Your ward was Present on this date.");
 }
 
 else {
  $response = new Twiml();
-$message = $response->message("Incorrect Format. Try date(yyyy-mm-dd)");
+$message = $response->message("Incorrect Format. Try date(yyyy-mm-dd) attendance and Roll No.");
 }
 
 echo $response;
