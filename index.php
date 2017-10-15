506a3deaef3cbf60ce961779bd3b06d9a5d4c3d8
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 17:19:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use Twilio\Twiml;
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
 $from1 = substr($from, -2);
+$regnum = 78;
 
 if( ($from == '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
@@ -19,12 +20,12 @@ else if( ($from == '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) &
 $message = $response->message("Your ward was Absent on this date.");
 }
 
-else if( (stripos($from1, '78' )!== FALSE) && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+else if( (stripos($from1, $regnum )!== FALSE) && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
 }
 
-else if( ($from1 !== '78') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+else if( ($from1 !== 78) && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
 }
