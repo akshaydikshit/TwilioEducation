ad9b600c64b43985b542006eebae2ebacba636a9
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 17:24:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once './autoload.php';
 use Twilio\Twiml;
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
-$from1 = substr($from, -2);
-$regnum = 78;
+$from1 = substr($from, -1);
+$regnum = 8;
 
 if( ($from == '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
