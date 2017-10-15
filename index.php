c68a3cce341f8f20bb91988b0ff882e7d8361eb1
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 17:28:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,12 +20,12 @@ else if( ($from == '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) &
 $message = $response->message("Your ward was Absent on this date.");
 }
 
-else if( (stripos($from1, $regnum )!== FALSE) && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+elseif( (stripos($from1, $regnum )!== FALSE) && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
 }
 
-else if( ($from == '+16464645046') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+elseif( ($from == '+16464645046') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
 }
