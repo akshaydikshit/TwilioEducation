6da0ca3c2efe61e829d91e77b7598463925f9d71
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 16:46:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
 <?php
 require_once './autoload.php';
 use Twilio\Twiml;
-
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
-
 if( ($from == '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Your ward was Present on this date.");
 }
-
-else if( $body == 'bye' ){
-
-  $response = new Twiml();
-$message = $response->message("GoodBye!");
+else if( ($from == '+19292402278') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Your ward was Present on this date.");
 }
 
+else if( ($from == '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Your ward was Absent on this date.");
+}
+
+else if( ($from != '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
+}
+
+else if( ($from != '+19292402278') && (stripos($body, '2017-10-02' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
+}
+
+else if( ($from != '+19292402278') && (stripos($body, '2017-10-03' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Sorry this is not a registered number. Please try again with a registered number.");
+}
+
+else if( ($from == '+19292402278') && (stripos($body, '2017-10-04' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+ $response = new Twiml();
+$message = $response->message("Your ward was Absent on this date.");
+}
 
 echo $response;
-
-
 ?>
