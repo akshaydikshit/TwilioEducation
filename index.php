54cbba6f78ac1b302345d03d8ad6ad4ee7ec9b1b
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 14:50:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once './autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-$message = $response->message();
+$message = $response->message("Thanks for the text.");
+
 $message->body('Hello Worlddsadsadsadas!');
 $response->redirect('https://demo.twilio.com/sms/welcome');
 
