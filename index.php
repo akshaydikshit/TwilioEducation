ae3fce36f9bc4ed4bd7cbff1bc151f2c94001cb4
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 15:29:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Twilio\Twiml;
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
 
-if( ($from == '+19292402278') && (stripos($body, '10/10/2017' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
+if( ($from == '+19292402278') && (stripos($body, '2017-10-01' )!== FALSE) && (stripos($body, 'attendance' )!== FALSE)){
  $response = new Twiml();
 $message = $response->message("Your ward was Present on this date.");
 }
