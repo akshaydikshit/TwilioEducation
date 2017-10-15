b455b7dddb626bb73f445c48e81e112473e7dc99
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 14:20:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
+SDK Version: 5.x
 <?php
 require_once './autoload.php';
 use Twilio\Twiml;
 
+$response = new Twiml();
+$message = $response->message();
 
 $body = $_REQUEST['Body'];
-
 if( $body == 'hello' ){
     $response->message('Hi!');
 }else if( $body == 'bye' ){
     $response->message('Goodbye');
 }
+
 echo $response;
+
+
 
 ?>
