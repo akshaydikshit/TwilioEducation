bc85930ee012e95ed18313bee5dfcd6b8fbccfa4
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-10-15 15:10:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Twilio\Twiml;
 $body = $_REQUEST['Body'];
 $from = $_REQUEST['From'];
 
-if( $body == 'hello' AND $from == '+19292402278' ){
+if( $from == '+19292402278' ){
 
   $response = new Twiml();
 $message = $response->message("Thanks for the text.");
